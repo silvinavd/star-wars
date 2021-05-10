daa9117337a26f4757b31439e88074fa80c41f2b
silvinavd <silvinavalverde8@gmail.com>
2021-05-10 19:00:47 +0000

    repuesto

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,38 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<div>
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<a className="navbar-brand" href="#">
+					Navbar
+				</a>
+				
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								href="#"
+								id="navbarDropdownMenuLink"
+								role="button"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false">
+								Dropdown link
+							</a>
+							<div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+								<a className="dropdown-item" href="#">
+									Action
+								</a>
+								<a className="dropdown-item" href="#">
+									Another action
+								</a>
+								<a className="dropdown-item" href="#">
+									Something else here
+								</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
 	);
 };
