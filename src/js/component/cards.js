464a41ab36c5f77c2d31bc9eb338940b0909213d
silvinavd <silvinavalverde8@gmail.com>
2021-05-11 13:05:43 +0000

    personajes

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 export const Cards = () => {
 	return (
 		<div>
-			<h1 className="text-danger">Character</h1>
-			<div className="card-deck m-2">
 				<div className="card">
 					<img src="http://www.servithermic.cl/images/400X200.gif" className="card-img-top" alt="..." />
 					<div className="card-body">
@@ -23,40 +21,7 @@ export const Cards = () => {
 						</button>
 					</div>
 				</div>
-				<div className="card">
-					<img src="http://www.servithermic.cl/images/400X200.gif" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h4 className="card-title">Darth Vader</h4>
-						<p className="card-text">
-							Gender: male <br />
-							Hair Color: none <br />
-							Eye-Color: yellow
-						</p>
-						<button type="button" className="btn btn-outline-primary btn-lg">
-							Learn more!
-						</button>
-						<button type="button" className="btn btn-outline-warning btn-lg float-right">
-							<i className="fas fa-heart" />
-						</button>
-					</div>
-				</div>
-				<div className="card">
-					<img src="http://www.servithermic.cl/images/400X200.gif" className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h4 className="card-title">Darth Vader</h4>
-						<p className="card-text">
-							Gender: male <br />
-							Hair Color: none <br />
-							Eye-Color: yellow
-						</p>
-						<button type="button" className="btn btn-outline-primary btn-lg">
-							Learn more!
-						</button>
-						<button type="button" className="btn btn-outline-warning btn-lg float-right">
-							<i className="fas fa-heart" />
-						</button>
-					</div>
-				</div>
+				
 			</div>
 		</div>
 	);
--- src/js/layout.js
+++ src/js/layout.js
@@ -12,35 +12,34 @@ import { Footer } from "./component/footer";
 import { Cards } from "./component/cards";
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+    //the basename is used when your project is published in a subdirectory and not in the root of the domain
+    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+    const basename = process.env.BASENAME || "";
 
-	return (
-		<div className="d-flex flex-column">
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Cards />
-					<Switch>
-						<Route exact path="/">
+    return (
+        <div className="d-flex flex-column">
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <Navbar />
+                    <Switch>
+                        {/*<Route exact path="/">
 							<Home />
-						</Route>
-						<Route exact path="/demo">
-							<Demo />
-						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
-						</Route>
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
-					</Switch>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+    </Route>*/}
+                        <Route exact path="/">
+                            <Demo />
+                        </Route>
+                        <Route exact path="/single/:theid">
+                            <Single />
+                        </Route>
+                        <Route>
+                            <h1>Not found!</h1>
+                        </Route>
+                    </Switch>
+                    <Footer />
+                </ScrollToTop>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default injectContext(Layout);
